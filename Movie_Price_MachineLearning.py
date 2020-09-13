29740cd461387c88d7df6047cc557cc35fdd72c1
Andrew Kim <akakimmykim@gmail.com>
2020-09-14 04:19:41 +0900

    Harvard-EdX Python_for_Research_MachineLearning_Practice

--- Movie_Price_MachineLearning.py
+++ Movie_Price_MachineLearning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
@@ -44,7 +44,8 @@ for genre in genre_list:
     for row in df.index:
         if (genre in df['genres'][row]):
             df.loc[row, genre] = 1
-            
+
+"""            
 # Statistical Analysis
 continuous_covariates = ['budget', 'popularity', 'runtime', 'vote_count', 'vote_average']
 outcomes_and_continuous_covariates = continuous_covariates + [regression_target, classification_target]
@@ -52,8 +53,96 @@ plotting_variables = ['budget', 'popularity', regression_target]
 axes = pd.plotting.scatter_matrix(df[plotting_variables], alpha=0.15, \
        color=(0,0,0), hist_kwds={"color":(0,0,0)}, facecolor=(1,0,0))
     
-#plt.show()
+plt.show()
 for covariate in outcomes_and_continuous_covariates:
     for row in df.index:
         df.loc[row, covariate] = np.log10(1+df.loc[row, covariate])
 print(df[outcomes_and_continuous_covariates].skew())
+"""
+
+
+# Part 2 Statistical Analysis
+all_covariates = ['budget', 'popularity', 'runtime', 'vote_count', 'vote_average', 'Action', 'Adventure', 'Fantasy', 
+                  'Science Fiction', 'Crime', 'Drama', 'Thriller', 'Animation', 'Family', 'Western', 'Comedy', 'Romance', 
+                  'Horror', 'Mystery', 'War', 'History', 'Music', 'Documentary', 'TV Movie', 'Foreign']
+
+regression_outcome = df[regression_target]
+classification_outcome = df[classification_target]
+covariates = df[all_covariates]
+
+linear_regression = LinearRegression()
+logistic_regression = LogisticRegression()
+forest_regression = RandomForestRegressor(max_depth=4, random_state=0)
+forest_classifier = RandomForestClassifier(max_depth=4, random_state=0)
+
+def correlation(estimator, X, y):
+    predictions = estimator.fit(X,y).predict(X)
+    return r2_score(y, predictions)
+
+def accuracy(estimator, X, y):
+    predictions = estimator.fit(X,y).predict(X)
+    return accuracy_score(y, predictions)
+
+linear_regression_scores = cross_val_score(linear_regression, covariates, regression_outcome, cv=10, scoring=correlation)
+forest_regression_scores = cross_val_score(forest_regression, covariates, regression_outcome, cv=10, scoring=correlation)
+
+# Plot Results
+plt.axes().set_aspect('equal', 'box')
+plt.scatter(linear_regression_scores, forest_regression_scores)
+plt.plot((0, 1), (0, 1), 'k-')
+
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel("Linear Regression Score")
+plt.ylabel("Forest Regression Score")
+
+plt.show()
+
+logistic_regression_scores = cross_val_score(logistic_regression, covariates, classification_outcome, cv=10, scoring=accuracy)
+forest_classification_scores = cross_val_score(forest_classifier, covariates, classification_outcome, cv=10, scoring=accuracy)
+
+plt.axes().set_aspect('equal', 'box')
+plt.scatter(logistic_regression_scores, forest_classification_scores)
+plt.plot((0, 1), (0, 1), 'k-')
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel("Linear Classification Score")
+plt.ylabel("Forest Classification Score")
+
+plt.show()
+
+
+# Part 3 Statistical Analysis
+
+positive_revenue_df = df[df['profitable']==1]
+
+regression_outcome = positive_revenue_df[regression_target]
+classification_outcome = positive_revenue_df[classification_target]
+covariates = positive_revenue_df[all_covariates]
+
+# Reinstantiate all regression models and classifiers.
+linear_regression = LinearRegression()
+logistic_regression = LogisticRegression()
+forest_regression = RandomForestRegressor(max_depth=4, random_state=0)
+forest_classifier = RandomForestClassifier(max_depth=4, random_state=0)
+linear_regression_scores = cross_val_score(linear_regression, covariates, regression_outcome, cv=10, scoring=correlation)
+forest_regression_scores = cross_val_score(forest_regression, covariates, regression_outcome, cv=10, scoring=correlation)
+logistic_regression_scores = cross_val_score(logistic_regression, covariates, classification_outcome, cv=10, scoring=accuracy)
+forest_classification_scores = cross_val_score(forest_classifier, covariates, classification_outcome, cv=10, scoring=accuracy)
+
+print(forest_regression_scores.mean())
+
+plt.axes().set_aspect('equal', 'box')
+plt.scatter(logistic_regression_scores, forest_classification_scores)
+plt.plot((0, 1), (0, 1), 'k-')
+
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel("Linear Classification Score")
+plt.ylabel("Forest Classification Score")
+
+plt.show()
+
+# Print the importance of each covariate in the random forest classification.
+print(forest_classifier.fit(positive_revenue_df[all_covariates], classification_outcome))
+print(sorted(list(zip(all_covariates, forest_classifier.feature_importances_)), key=lambda tup: tup[1]))
